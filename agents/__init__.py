1173e921af965d4cb21f1db5464b8da044e5e74b
SJ <80259014+lvalue-dev@users.noreply.github.com>
2026-05-17 18:02:44 +0900

    feat: 거래량/기관 에이전트 추가, 뉴스 강화, 5자 토론 구조 개선

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,8 @@
 from agents.market_analyst import market_analyst_node
 from agents.news_analyst import news_analyst_node
 from agents.stock_screener import stock_screener_node
+from agents.volume_agent import volume_agent_node
+from agents.institutional_agent import institutional_agent_node
 from agents.supervisor import supervisor_node, debate_agent_node, make_final_decision
 from agents.trading_agent import trading_agent_node
 
@@ -8,6 +10,8 @@ __all__ = [
     "market_analyst_node",
     "news_analyst_node",
     "stock_screener_node",
+    "volume_agent_node",
+    "institutional_agent_node",
     "supervisor_node",
     "debate_agent_node",
     "make_final_decision",
